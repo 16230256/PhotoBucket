9a35513731d54fc0c79e4f2d3df58d4e6c8c6416
16230256 <39533070+16230256@users.noreply.github.com>
2018-11-13 19:17:50 +0000

    photo phase 1

---
+++ PhotoBucket/app/src/main/java/ie/ul/deirdreshanahan/photobucket/Constants.java
@@ -0,0 +1,11 @@
+package ie.ul.deirdreshanahan.photobucket;
+
+public class Constants {
+
+    public static final String TAG = "MQ";
+    public static final String COLLECTION_PATH = "photobucket";
+    public static final String KEY_QUOTE = "caption";
+    public static final String KEY_MOVIE = "imageUrl";
+    public static final String KEY_CREATED = "created";
+    public static final String EXTRA_DOC_ID = "document_id";
+}
